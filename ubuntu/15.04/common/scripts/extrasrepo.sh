a30e748bcee628b4c4f29bb88f64393dfdf67bcc
Diego Gutierrez <dgutierrez1287@gmail.com>
2016-06-05 05:13:18 -0400

    MODIFIED: shifted ubuntu desktop templates and deleted 15.04 templates and configs

--- ubuntu/15.04/common/scripts/extrasrepo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3E5C1192
-
-apt-get update
--- ubuntu/15.04/common/scripts/puppet.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-wget https://apt.puppetlabs.com/puppetlabs-release-pc1-vivid.deb
-
-dpkg -i puppetlabs-release-trusty.deb
-
-apt-get update
-
-apt-get install -y puppet puppetmaster facter
